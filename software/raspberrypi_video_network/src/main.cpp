50a0588e7ddce6cdd9abe77097a7883dff846760
AnujN <natraj.anuj@gmail.com>
2024-08-06 10:06:32 -0500

    Formatting

--- software/raspberrypi_video_network/src/main.cpp
+++ software/raspberrypi_video_network/src/main.cpp
@@ -22,8 +22,9 @@
 
 /// \brief Function to describe how to use the command line arguments
 /// \param cmd Argument of the command line, here it is the program
-void printUsage(char *cmd) {
-        char *cmdname = basename(cmd);
+void printUsage(char *cmd)
+{
+	char *cmdname = basename(cmd);
 	printf("Usage: %s [OPTION]...\n"
                " -h      display this help and exit\n"
                " -net x  set the ip address (default: 10.42.0.1)\n"
@@ -35,7 +36,7 @@ void printUsage(char *cmd) {
 /// \param argc Number of command-line arguments.
 /// \param argv Array of command-line arguments.
 /// \return 0 if successful, -1 if failure.
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
 	int typeLepton = 3; // Lepton 3.x
 	unsigned int spiSpeed = 20 * 1000 * 1000; // SPI bus speed 20MHz
@@ -53,16 +54,19 @@ int main( int argc, char **argv )
     struct sockaddr_in servaddr;
 	const char *netIP = "10.42.0.1";
 
-	for(int i=1; i < argc; i++) {
-		if (strcmp(argv[i], "-h") == 0) {
+	for(int i=1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "-h") == 0)
+		{
 			printUsage(argv[0]);
 			exit(0);
 		}
-		else if (strcmp(argv[i], "-net") == 0) {
-			if (i + 1 != argc){
+		else if (strcmp(argv[i], "-net") == 0)
+		{
+			if (i + 1 != argc)
+			{
 				netIP = argv[++i];
-			}
-			else {
+			} else {
 				std::cerr << "Error: Enter an IP." << std::endl;
 				exit(1);
 			}
@@ -72,7 +76,8 @@ int main( int argc, char **argv )
 	SpiOpenPort(0, spiSpeed);
 
 	// Setting up UDP socket
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
         std::cerr << "Socket creation failed" << std::endl;
         return -1;
     }
@@ -84,22 +89,25 @@ int main( int argc, char **argv )
     servaddr.sin_port = htons(PORT);
     servaddr.sin_addr.s_addr = inet_addr(netIP);
 
-	while(true) {
-
+	while(true)
+	{
 		//read data packets from lepton over SPI
 		int resets = 0;
 		int segmentNumber = -1;
-		for(int j=0;j<PACKETS_PER_FRAME;j++) {
+		for(int j=0;j<PACKETS_PER_FRAME;j++)
+		{
 			//if it's a drop packet, reset j to 0, set to -1 so he'll be at 0 again loop
 			read(spi_cs0_fd, result+sizeof(uint8_t)*PACKET_SIZE*j, sizeof(uint8_t)*PACKET_SIZE);
 			int packetNumber = result[j*PACKET_SIZE+1];
-			if(packetNumber != j) {
+			if(packetNumber != j)
+			{
 				j = -1;
 				resets += 1;
 				usleep(1000);
 				//Note: we've selected 750 resets as an arbitrary limit, since there should never be 750 "null" packets between two valid transmissions at the current poll rate
 				//By polling faster, developers may easily exceed this count, and the down period between frames may then be flagged as a loss of sync
-				if(resets == 750) {
+				if(resets == 750)
+				{
 					SpiClosePort(0);
 					lepton_reboot();
 					n_wrong_segment = 0;
@@ -109,9 +117,11 @@ int main( int argc, char **argv )
 				}
 				continue;
 			}
-			if (packetNumber == 20) {
+			if (packetNumber == 20)
+			{
 				segmentNumber = (result[j*PACKET_SIZE] >> 4) & 0x0f;
-				if ((segmentNumber < 1) || (4 < segmentNumber)) {
+				if ((segmentNumber < 1) || (4 < segmentNumber))
+				{
 					break;
 				}
 			}
@@ -119,22 +129,27 @@ int main( int argc, char **argv )
 
 		int iSegmentStart = 1;
 		int iSegmentStop;
-		if ((segmentNumber < 1) || (4 < segmentNumber)) {
+		if ((segmentNumber < 1) || (4 < segmentNumber))
+		{
 			n_wrong_segment++;
 			continue;
 		}
-		if (n_wrong_segment != 0) {
+		if (n_wrong_segment != 0)
+		{
 			n_wrong_segment = 0;
 		}
 		memcpy(shelf[segmentNumber - 1], result, sizeof(uint8_t) * PACKET_SIZE*PACKETS_PER_FRAME);
-		if (segmentNumber != 4) {
+		if (segmentNumber != 4)
+		{
 			continue;
 		}
 		iSegmentStop = 4;
 
-		for (int i = 0; i < 4; ++i) {
+		for (int i = 0; i < 4; ++i)
+		{
             ssize_t sent_bytes = sendto(sockfd, shelf[i], sizeof(shelf[i]), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-            if (sent_bytes < 0) {
+            if (sent_bytes < 0)
+			{
                 std::cerr << "Send failed" << std::endl;
                 close(sockfd);
                 return -1;
@@ -142,7 +157,6 @@ int main( int argc, char **argv )
         }
 		usleep(10000);
 	}
-	
 	// Close the socket
 	close(sockfd);
 	//finally, close SPI port just bcuz
